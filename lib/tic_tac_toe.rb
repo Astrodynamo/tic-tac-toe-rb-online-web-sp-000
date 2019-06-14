e814f8a1cfc7e2dac638610bf7da7d4eab74c913
Amanda King <amapickell@gmail.com>
2019-06-14 00:44:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,12 +1,12 @@
 WIN_COMBINATIONS = [
-    [0,1,2], # Top
-    [3,4,5], # Middle
-    [6,7,8], # Bottom
-    [0,3,6], # Left
-    [1,4,7], # Middle
-    [2,5,8], # Right
-    [0,4,8], # back diagonal
-    [2,4,6] # forward diagonal
+    [0,1,2], # Top row
+    [3,4,5], # Middle row
+    [6,7,8], # Bottom row
+    [0,3,6], # Left column
+    [1,4,7], # Middle column
+    [2,5,8], # Right column
+    [0,4,8], # diagonal from top left
+    [2,4,6] # diagonal from top right
   ]
   
   
